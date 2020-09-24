41118b2a339b46ca0c37bb7c438bfeb1efe0e2cc
Andrii Koliada <andykoliada@gmail.com>
2020-09-24 09:57:42 +0300

    init

---
+++ MaxSumFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MaxSumFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
