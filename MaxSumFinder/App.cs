45db04f788c0ddd58960b049f8b5e028f9256478
Andrii Koliada <andykoliada@gmail.com>
2020-09-29 13:03:40 +0300

    new DI approach

---
+++ MaxSumFinder/App.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace MaxSumFinder
+{
+    class App
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello from App.cs");
+        }
+    }
+}
--- MaxSumFinder/Program.cs
+++ MaxSumFinder/Program.cs
@@ -6,36 +6,67 @@ namespace MaxSumFinder
 {
     class Program
     {
-        private static IServiceProvider serviceProvider;
-
         static void Main(string[] args)
         {
-            RegisterServices();
-            var finder = new MaxSumFinder(serviceProvider);
-            if (args.Length != 0)
-            {
+            var services = ConfigureServices();
 
-                finder.Run(args);
-            }
-            else
-            {
-                finder.Run();
-            }
+            var serviceProvider = services.BuildServiceProvider();
 
-
+            // calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
         }
 
-        private static void RegisterServices()
+        private static IServiceCollection ConfigureServices()
         {
-            //setting up DI (registering services)
-            var collection = new ServiceCollection()
-                .AddSingleton<IInputPromt, ConsoleInputPromt>()
-                .AddSingleton<IFileReader, FileReader>()
-                .AddSingleton<IFileProcessor, FileProcessor>()
-                .AddSingleton<IPrinter, ConsoleOutput>();
-            serviceProvider = collection.BuildServiceProvider();
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            // required to run the application
+            services.AddTransient<App>();
+
+            return services;
         }
-    }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+    //    private static IServiceProvider serviceProvider;
+
+    //    static void Main(string[] args)
+    //    {
+    //        RegisterServices();
+    //        var finder = new MaxSumFinder(serviceProvider);
+    //        if (args.Length != 0)
+    //        {
+
+    //            finder.Run(args);
+    //        }
+    //        else
+    //        {
+    //            finder.Run();
+    //        }
+
+
+    //    }
+
+    //    private static void RegisterServices()
+    //    {
+    //        //setting up DI (registering services)
+    //        var collection = new ServiceCollection()
+    //            .AddSingleton<IInputPromt, ConsoleInputPromt>()
+    //            .AddSingleton<IFileReader, FileReader>()
+    //            .AddSingleton<IFileProcessor, FileProcessor>()
+    //            .AddSingleton<IPrinter, ConsoleOutput>();
+    //        serviceProvider = collection.BuildServiceProvider();
+    //    }
+    //}
 }
 
 //Предоставляется файл (путь к файлу необходимо или запросить у пользователя при старте программы,
